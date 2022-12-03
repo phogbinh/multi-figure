2b21e584d30beb4183ed3b5eba06908d110db5f9
HSCC MATLAB <hsccml-dual2080ti@hscc.nthu.edu.tw>
2022-12-03 14:42:23 +0800

    [master] added simulation eps.

---
+++ update_eps.sh
@@ -0,0 +1,8 @@
+SIMULATION_DIR=${MULTI_DIR}simulation/
+
+cp ${SIMULATION_DIR}experiments_optimality/execution_time.eps ./optimality_execution_time.eps
+cp ${SIMULATION_DIR}experiments_optimality/total_average_rates.eps ./optimality_total_average_rates.eps
+cp ${SIMULATION_DIR}experiments_optimality/punctured_costs.eps ./optimality_punctured_costs.eps
+cp ${SIMULATION_DIR}experiments_heuristics/execution_time.eps ./heuristics_execution_time.eps
+cp ${SIMULATION_DIR}experiments_heuristics/total_average_rates.eps ./heuristics_total_average_rates.eps
+cp ${SIMULATION_DIR}experiments_heuristics/punctured_costs.eps ./heuristics_punctured_costs.eps
