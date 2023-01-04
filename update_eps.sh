4f1a673ec837ac89dffb3eab9b0e9e09586e9a0d
HSCC MATLAB <hsccml-dual2080ti@hscc.nthu.edu.tw>
2023-01-04 10:16:46 +0800

    [master] added baseline fairness and sum log utility.

--- update_eps.sh
+++ update_eps.sh
@@ -1,6 +1,8 @@
 SIMULATION_DIR=${MULTI_DIR}simulation/
 
 cp ${SIMULATION_DIR}experiments_heuristics/total_average_rates.eps ./baseline_total_average_rates.eps
+cp ${SIMULATION_DIR}experiments_heuristics/fairness.eps ./baseline_fairness.eps
+cp ${SIMULATION_DIR}experiments_heuristics/sum_log_utility.eps ./baseline_sum_log_utility.eps
 cp ${SIMULATION_DIR}experiments_heuristics/punctured_costs.eps ./baseline_punctured_costs.eps
 cp ${SIMULATION_DIR}experiments_optimality/execution_time.eps ./optimal_execution_time.eps
 cp ${SIMULATION_DIR}experiments_optimality/punctured_costs.eps ./optimal_punctured_costs.eps
