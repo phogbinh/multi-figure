d7279200bf9e594eefbdbff12d8e84bb0be38d24
HSCC MATLAB <hsccml-dual2080ti@hscc.nthu.edu.tw>
2023-01-04 10:01:46 +0800

    [master] renamed to baseline and optimal.

--- update_eps.sh
+++ update_eps.sh
@@ -1,6 +1,6 @@
 SIMULATION_DIR=${MULTI_DIR}simulation/
 
-cp ${SIMULATION_DIR}experiments_heuristics/total_average_rates.eps ./heuristics_total_average_rates.eps
-cp ${SIMULATION_DIR}experiments_heuristics/punctured_costs.eps ./heuristics_punctured_costs.eps
-cp ${SIMULATION_DIR}experiments_optimality/execution_time.eps ./optimality_execution_time.eps
-cp ${SIMULATION_DIR}experiments_optimality/punctured_costs.eps ./optimality_punctured_costs.eps
+cp ${SIMULATION_DIR}experiments_heuristics/total_average_rates.eps ./baseline_total_average_rates.eps
+cp ${SIMULATION_DIR}experiments_heuristics/punctured_costs.eps ./baseline_punctured_costs.eps
+cp ${SIMULATION_DIR}experiments_optimality/execution_time.eps ./optimal_execution_time.eps
+cp ${SIMULATION_DIR}experiments_optimality/punctured_costs.eps ./optimal_punctured_costs.eps
