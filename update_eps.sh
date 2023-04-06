fe902c4005d3503ab9e7b5f85acb95e8d5d40f41
HSCC MATLAB <hsccml-dual2080ti@hscc.nthu.edu.tw>
2023-04-06 19:00:18 +0800

    [master] added baseline/heuristic execution time and associated urllc users nums update.

--- update_eps.sh
+++ update_eps.sh
@@ -3,10 +3,14 @@ SIMULATION_DIR=${MULTI_DIR}simulation/
 cp ${SIMULATION_DIR}experiments_baseline/total_average_rates.eps ./baseline_total_average_rates.eps
 cp ${SIMULATION_DIR}experiments_baseline/fairness.eps ./baseline_fairness.eps
 cp ${SIMULATION_DIR}experiments_baseline/sum_log_utility.eps ./baseline_sum_log_utility.eps
+cp ${SIMULATION_DIR}experiments_baseline/execution_time.eps ./baseline_execution_time.eps
+cp ${SIMULATION_DIR}experiments_baseline/associated_urllc_users_nums.eps ./baseline_associated_urllc_users_nums.eps
 cp ${SIMULATION_DIR}experiments_baseline/punctured_costs.eps ./baseline_punctured_costs.eps
 cp ${SIMULATION_DIR}experiments_optimal/execution_time.eps ./optimal_execution_time.eps
 cp ${SIMULATION_DIR}experiments_optimal/punctured_costs.eps ./optimal_punctured_costs.eps
 cp ${SIMULATION_DIR}experiments_heuristic/total_average_rates.eps ./heuristic_total_average_rates.eps
 cp ${SIMULATION_DIR}experiments_heuristic/fairness.eps ./heuristic_fairness.eps
 cp ${SIMULATION_DIR}experiments_heuristic/sum_log_utility.eps ./heuristic_sum_log_utility.eps
+cp ${SIMULATION_DIR}experiments_heuristic/execution_time.eps ./heuristic_execution_time.eps
+cp ${SIMULATION_DIR}experiments_heuristic/associated_urllc_users_nums.eps ./heuristic_associated_urllc_users_nums.eps
 cp ${SIMULATION_DIR}experiments_heuristic/punctured_costs.eps ./heuristic_punctured_costs.eps
