5866e7a031e4daf842da887e6bbcc089844fe728
HSCC MATLAB <hsccml-2080sp@hscc.nthu.edu.tw>
2023-04-11 10:23:01 +0800

    [master] replaced update baseline with feasibility and optimality.

--- update_eps.sh
+++ update_eps.sh
@@ -1,8 +1,8 @@
 SIMULATION_DIR=${MULTI_DIR}simulation/
 
-cp ${SIMULATION_DIR}experiments_baseline/total_average_rates.eps ./baseline_total_average_rates.eps
-cp ${SIMULATION_DIR}experiments_baseline/fairness.eps ./baseline_fairness.eps
-cp ${SIMULATION_DIR}experiments_baseline/sum_log_utility.eps ./baseline_sum_log_utility.eps
-cp ${SIMULATION_DIR}experiments_baseline/execution_time.eps ./baseline_execution_time.eps
-cp ${SIMULATION_DIR}experiments_baseline/associated_urllc_users_nums.eps ./baseline_associated_urllc_users_nums.eps
-cp ${SIMULATION_DIR}experiments_baseline/punctured_costs.eps ./baseline_punctured_costs.eps
+cp ${SIMULATION_DIR}experiments_feasibility/associated_urllc_users_nums.eps ./baseline_associated_urllc_users_nums.eps
+cp ${SIMULATION_DIR}experiments_optimality/total_average_rates.eps ./baseline_total_average_rates.eps
+cp ${SIMULATION_DIR}experiments_optimality/fairness.eps ./baseline_fairness.eps
+cp ${SIMULATION_DIR}experiments_optimality/sum_log_utility.eps ./baseline_sum_log_utility.eps
+cp ${SIMULATION_DIR}experiments_optimality/execution_time.eps ./baseline_execution_time.eps
+cp ${SIMULATION_DIR}experiments_optimality/punctured_costs.eps ./baseline_punctured_costs.eps
