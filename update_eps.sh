8696263cbd65b2a092c61205097ec6441b0950d7
HSCC MATLAB <hsccml-dual2080ti@hscc.nthu.edu.tw>
2023-04-23 13:26:34 +0800

    [master] added feasibility associated urllc users nums.

--- update_eps.sh
+++ update_eps.sh
@@ -1,5 +1,6 @@
 SIMULATION_DIR=${MULTI_DIR}simulation/
 
+cp ${SIMULATION_DIR}experiments_feasibility/associated_urllc_users_nums.eps ./feasibility_associated_urllc_users_nums.eps
 cp ${SIMULATION_DIR}experiments_optimality/total_average_rates.eps ./optimality_total_average_rates.eps
 cp ${SIMULATION_DIR}experiments_optimality/fairness.eps ./optimality_fairness.eps
 cp ${SIMULATION_DIR}experiments_optimality/sum_log_utility.eps ./optimality_sum_log_utility.eps
