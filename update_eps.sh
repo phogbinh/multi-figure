3e821d991519a2466ab65c68494e2e4b1ce2741d
HSCC MATLAB <hsccml-1080ti@hscc.nthu.edu.tw>
2023-05-18 21:28:53 +0800

    [master] renamed optimality to casual.

--- update_eps.sh
+++ update_eps.sh
@@ -1,9 +1,9 @@
 SIMULATION_DIR=${MULTI_DIR}simulation/
 
 cp ${SIMULATION_DIR}experiments_busy/associated_urllc_users_nums.eps ./busy_associated_urllc_users_nums.eps
-cp ${SIMULATION_DIR}experiments_optimality/total_average_rates.eps ./optimality_total_average_rates.eps
-cp ${SIMULATION_DIR}experiments_optimality/fairness.eps ./optimality_fairness.eps
-cp ${SIMULATION_DIR}experiments_optimality/sum_log_utility.eps ./optimality_sum_log_utility.eps
-cp ${SIMULATION_DIR}experiments_optimality/execution_time.eps ./optimality_execution_time.eps
-cp ${SIMULATION_DIR}experiments_optimality/punctured_costs.eps ./optimality_punctured_costs.eps
+cp ${SIMULATION_DIR}experiments_casual/total_average_rates.eps ./casual_total_average_rates.eps
+cp ${SIMULATION_DIR}experiments_casual/fairness.eps ./casual_fairness.eps
+cp ${SIMULATION_DIR}experiments_casual/sum_log_utility.eps ./casual_sum_log_utility.eps
+cp ${SIMULATION_DIR}experiments_casual/execution_time.eps ./casual_execution_time.eps
+cp ${SIMULATION_DIR}experiments_casual/punctured_costs.eps ./casual_punctured_costs.eps
 cp ${SIMULATION_DIR}experiments_sparse/punctured_costs.eps ./sparse_punctured_costs.eps
