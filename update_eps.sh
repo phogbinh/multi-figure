f7195ac20493c1e235a433b13e1d5f00668691d3
HSCC MATLAB <hsccml-1080ti@hscc.nthu.edu.tw>
2023-05-27 18:50:35 +0800

    [master] added optimal.

--- update_eps.sh
+++ update_eps.sh
@@ -1,5 +1,7 @@
 SIMULATION_DIR=${MULTI_DIR}simulation/
 
+cp ${SIMULATION_DIR}experiments_optimal/execution_time.eps ./optimal_execution_time.eps
+cp ${SIMULATION_DIR}experiments_optimal/punctured_costs.eps ./optimal_punctured_costs.eps
 cp ${SIMULATION_DIR}experiments_busy/associated_urllc_users_nums.eps ./busy_associated_urllc_users_nums.eps
 cp ${SIMULATION_DIR}experiments_casual/total_average_rates.eps ./casual_total_average_rates.eps
 cp ${SIMULATION_DIR}experiments_casual/fairness.eps ./casual_fairness.eps
