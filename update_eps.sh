9f81591ce720f8f38cafe8d2b15db4d37b4779af
HSCC MATLAB <hsccml-dual2080ti@hscc.nthu.edu.tw>
2023-07-10 23:55:19 +0800

    [master] removed casual fairness.

--- update_eps.sh
+++ update_eps.sh
@@ -4,7 +4,6 @@ cp ${SIMULATION_DIR}experiments_optimal/execution_time.eps ./optimal_execution_t
 cp ${SIMULATION_DIR}experiments_optimal/punctured_costs.eps ./optimal_punctured_costs.eps
 cp ${SIMULATION_DIR}experiments_busy/associated_urllc_users_nums.eps ./busy_associated_urllc_users_nums.eps
 cp ${SIMULATION_DIR}experiments_casual/total_average_rates.eps ./casual_total_average_rates.eps
-cp ${SIMULATION_DIR}experiments_casual/fairness.eps ./casual_fairness.eps
 cp ${SIMULATION_DIR}experiments_casual/sum_log_utility.eps ./casual_sum_log_utility.eps
 cp ${SIMULATION_DIR}experiments_casual/execution_time.eps ./casual_execution_time.eps
 cp ${SIMULATION_DIR}experiments_casual/punctured_costs.eps ./casual_punctured_costs.eps
