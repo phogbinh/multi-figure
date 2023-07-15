0ff5689ddf8d0d48a4f287c7fd3af6b176472dae
HSCC MATLAB <hsccml-dual2080ti@hscc.nthu.edu.tw>
2023-07-15 13:48:51 +0800

    [master] added experiments swapping.

--- update_eps.sh
+++ update_eps.sh
@@ -1,5 +1,7 @@
 SIMULATION_DIR=${MULTI_DIR}simulation/
 
+cp ${SIMULATION_DIR}experiments_swapping/execution_time.eps ./swapping_execution_time.eps
+cp ${SIMULATION_DIR}experiments_swapping/punctured_costs.eps ./swapping_punctured_costs.eps
 cp ${SIMULATION_DIR}experiments_optimal/execution_time.eps ./optimal_execution_time.eps
 cp ${SIMULATION_DIR}experiments_optimal/punctured_costs.eps ./optimal_punctured_costs.eps
 cp ${SIMULATION_DIR}experiments_busy/associated_urllc_users_nums.eps ./busy_associated_urllc_users_nums.eps
